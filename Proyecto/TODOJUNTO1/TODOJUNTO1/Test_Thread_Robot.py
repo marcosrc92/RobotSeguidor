7ccf3a73ee9725d34760fa9ee33adc64f2714ff7
marcosrc92 <47392581+marcosrc92@users.noreply.github.com>
2019-02-11 17:29:18 +0100

    Add files via upload

---
+++ Proyecto/TODOJUNTO1/TODOJUNTO1/Test_Thread_Robot.py
@@ -0,0 +1,45 @@
+import sys
+import time
+
+sys.path.append("C:\\Users\\marco\\OneDrive\\Documentos\\MAIIND\\Robotica\\Proyecto\\TODOJUNTO1\\TODOJUNTO1")
+from Thread_Robot import *
+sys.path.append("C:\\Users\\marco\\OneDrive\\Documentos\\MAIIND\\Robotica\\Proyecto\\TODOJUNTO1\\TODOJUNTO1")
+from classRobot_Arduino import *
+
+robot=Robot_Arduino('COM5','COM6') # creéation du robot
+robot.initialisation_Kangaroo() # initialisation du robot
+robot.initialisation_Arduino()
+
+thread = Thread_Robot(robot) # création du thread
+thread.start() # lancemement du thread
+
+while thread.connexion: # tant que la connexion n'est pas interompue
+
+    time.sleep(0.25)
+    
+    if thread.etat and thread.connexion: # bloque les ordres si demi-tour en cours
+
+        ordre = robot.arduino.readline().decode('utf-8').strip('\r\n')
+        
+        if ordre == "AVANCE":
+            print("ORDRE: " + ordre)
+            robot.vitesse_LigneDroite(0.5,0.25)
+            
+        if ordre == "RECULE":
+            print("ORDRE: " + ordre)
+            robot.vitesse_LigneDroite(-0.5,0.25)
+            
+        if ordre == "DROITE":
+            print(" COMMANDE: " + ordre)
+            robot.vitesse_Virage(45,0.25)
+            
+        if ordre == "GAUCHE":
+            print("ORDRE: " + ordre)
+            robot.vitesse_Virage(-45,0.25)
+
+        if ordre == "DEMI-TOUR":         
+            thread.etat = False
+            print("ORDRE: " + ordre)              
+            #self.robot.demiTour()
+            thread.etat=True
+            
